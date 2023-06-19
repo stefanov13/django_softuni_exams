ebc967d578af00a11ae84df80b9bedcc2a562a6b
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-06-19 23:48:22 +0300

    Add files via upload

--- car_collection/apps/cars/views.py
+++ car_collection/apps/cars/views.py
@@ -1,21 +1,57 @@
 from django.shortcuts import render, redirect
+from .models import Car
+from .forms import CarCreateForm, CarEditForm, CarDeleteForm
 
-# Create your views here.
+
 def car_create(request):
+    form = CarCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('catalogue')
+    
+    context = {
+        'form': form
+    }
 
-    return render(request, 'car/car-create.html')
-
-
-def car_details(request):
-
-    return render(request, 'car/car-details.html')
-
-
-def car_edit(request):
-
-    return render(request, 'car/car-edit.html')
+    return render(request, 'car/car-create.html', context=context)
 
 
-def car_delete(request):
+def car_details(request, pk):
+    car = Car.objects.filter(pk=pk).get()
 
-    return render(request, 'car/car-delete.html')
+    context = {
+        'car': car
+    }
+
+    return render(request, 'car/car-details.html', context=context)
+
+
+def car_edit(request, pk):
+    car = Car.objects.filter(pk=pk).get()
+    form = CarEditForm(request.POST or None, instance=car)
+    if form.is_valid():
+        form.save()
+        return redirect('catalogue')
+    
+    context = {
+        'form': form,
+        'car': car,
+    }
+
+    return render(request, 'car/car-edit.html', context=context)
+
+
+def car_delete(request, pk):
+    car = Car.objects.filter(pk=pk).get()
+    if request.method == 'GET':
+        form = CarDeleteForm(instance=car)
+    else:
+        car.delete()
+        return redirect('catalogue')
+    
+    context = {
+        'car': car,
+        'form': form
+    }
+
+    return render(request, 'car/car-delete.html', context=context)
