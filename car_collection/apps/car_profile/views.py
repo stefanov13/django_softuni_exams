f6d709937c269e9ec865caecfd9c652cacd7da6f
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-06-20 13:01:56 +0300

    Add files via upload

--- car_collection/apps/car_profile/views.py
+++ car_collection/apps/car_profile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ProfileCreateForm, ProfileEditForm
 from .models import Profile
+from ..cars.models import Car
 
 # Create your views here.
 def profile_create(request):
@@ -17,15 +18,38 @@ def profile_create(request):
 
 
 def profile_details(request):
+    all_cars = Car.objects.all()
 
-    return render(request, 'profile/profile-details.html')
+    total_price = sum(car.price for car in all_cars) 
+
+    context = {
+        'total_price': total_price,
+    }
+
+    return render(request, 'profile/profile-details.html', context=context)
 
 
 def profile_edit(request):
+    profile = Profile.objects.first()
+    form = ProfileEditForm(request.POST or None, instance=profile)
+    if form.is_valid():
+        form.save()
+        return redirect('profile-details')
+    
+    context = {
+    #   'profile': profile,      This is not necessary because in template we have a profile from template-tag in base.html
+        'form': form
+    }
 
-    return render(request, 'profile/profile-edit.html')
+    return render(request, 'profile/profile-edit.html', context=context)
 
 
 def profile_delete(request):
+    cars = Car.objects.all()
+    profile = Profile.objects.first()
+    if request.method == 'POST':
+        cars.delete()
+        profile.delete()
+        return redirect('index')
 
     return render(request, 'profile/profile-delete.html')
